116e5f9cf556d5d4ee386c0e1525ac8c3ecc7c4a
MrSuraj1 <surajyadavsy271@gmail.com>
2025-09-13 18:01:50 +0530

    Initial frontend commit

--- src/api.js
+++ src/api.js
@@ -1,8 +1,23 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "", 
+//   baseURL: "http://localhost:5000/api", // Express backend URL
+// });
+
+// export default api;
+
 import axios from "axios";
 
+// Detect if running on localhost or deployed site
+const baseURL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:5000/api"        // local dev
+    : "https://freelance-navsatech-backend-1.onrender.com/api"; // deployed backend
+
 const api = axios.create({
-  baseURL: "https://freelance-navsatech-backend-1.onrender.com/", 
-  baseURL: "http://localhost:5000/api", // Express backend URL
+  baseURL,
+  withCredentials: true, // if your backend uses cookies
 });
 
 export default api;
