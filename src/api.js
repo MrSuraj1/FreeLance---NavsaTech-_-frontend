e9624cd42e1995eb64250ad1807661da0f2d1265
MrSuraj1 <surajyadavsy271@gmail.com>
2025-09-13 18:10:31 +0530

    Initial frontend commit

--- src/api.js
+++ src/api.js
@@ -1,23 +1,13 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "", 
-//   baseURL: "http://localhost:5000/api", // Express backend URL
-// });
-
-// export default api;
-
 import axios from "axios";
 
-// Detect if running on localhost or deployed site
 const baseURL =
   window.location.hostname === "localhost"
-    ? "http://localhost:5000/api"        // local dev
-    : "https://freelance-navsatech-backend-1.onrender.com/api"; // deployed backend
+    ? "http://localhost:5000/api"
+    : "https://freelance-navsatech-backend-1.onrender.com/api";
 
 const api = axios.create({
   baseURL,
-  withCredentials: true, // if your backend uses cookies
+  withCredentials: true,
 });
 
 export default api;
