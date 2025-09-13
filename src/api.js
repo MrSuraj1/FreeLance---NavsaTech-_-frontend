9752899fac57269e6d0a63119c05f506872d08bb
MrSuraj1 <surajyadavsy271@gmail.com>
2025-09-14 00:25:14 +0530

    Initial frontend commit

--- src/api.js
+++ src/api.js
@@ -1,4 +1,9 @@
-const api = axios.create({
+import axios from "axios";
+
+// Use your Render backend URL
+const API = axios.create({
   baseURL: "https://freelance-navsatech-backend-1.onrender.com/api",
-  withCredentials: true, // only if backend supports cookies
+  withCredentials: true,  // optional, if backend supports cookies/sessions
 });
+
+export default API;
