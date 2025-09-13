f1b7a2c220a9e1c5b93ffba867dcec10f3657111
MrSuraj1 <surajyadavsy271@gmail.com>
2025-09-13 18:19:35 +0530

    Initial frontend commit

--- src/api.js
+++ src/api.js
@@ -1,13 +1,4 @@
-import axios from "axios";
-
-const baseURL =
-  window.location.hostname === "localhost"
-    ? "http://localhost:5000/api"
-    : "https://freelance-navsatech-backend-1.onrender.com/api";
-
 const api = axios.create({
-  baseURL,
-  withCredentials: true,
+  baseURL: "https://freelance-navsatech-backend-1.onrender.com/api",
+  withCredentials: true, // only if backend supports cookies
 });
-
-export default api;
